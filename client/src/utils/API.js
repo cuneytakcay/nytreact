982319749b3b6526891adb5e66b1f4a488af2cc0
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-28 14:27:29 -0700

    modified API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 const url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
 const apiKey = '&api-key=9d4a8986921972b65754ea0809d47c84%3A12%3A74623931'
-const query = {}
 
 export default {
   search: function(query) {
