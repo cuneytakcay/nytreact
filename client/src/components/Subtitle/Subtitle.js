2b3a71b0e127d33142b7a2587fe467888bab95a0
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-28 18:13:32 -0700

    Changed the name of the box component

---
+++ client/src/components/Subtitle/Subtitle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Subtitle extends React.Component {
+  render() {
+  	return (
+  		<div style={{ height: 100, clear: 'both' }}>
+    		{children}
+  		</div>
+  	)
+	}
+}
+
+export default Subtitle 
