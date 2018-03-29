8f8ea20a5aa0ec97fd2abab3d51dfc8aede709ca
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-29 14:57:37 -0700

    modified routes

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,12 +1,12 @@
 // Require dependencies
 const express = require('express')
-const articlesController = require('../../controllers/collectionController')
+const articlesController = require('../../controllers/articleController')
 
 const router = express.Router()
 
 // If rout is '/api/articles'
 router.route('/')
-  .get(articlesController.findAll)
+	.get(articlesController.findAll)
   .post(articlesController.create)
 
 module.exports = router
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,10 @@
 // Require dependencies
 const express = require('express')
 const articleRoutes = require('./articles')
-const collectionRoutes = require('./collection')
 
 const router = express.Router()
 
 // Article routes
 router.use('/articles', articleRoutes)
-
-// Collection routes
-router.use('/collection', collectionRoutes)
 
 module.exports = router
