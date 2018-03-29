4b212ebb7742ebbbf11ee740728b7b20d5d6e056
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-29 15:18:29 -0700

    added save and delete functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,19 @@ const url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
 const apiKey = '&api-key=ba44e7bc259943cb93ae5943f60d08fd'
 
 export default {
+	// Gets last 10 matching articles from NYT API
   getArticles: function(query) {
     return axios.get(url + query + apiKey)
-  }
+  }, 
+  // Gets all saved articles from MongoDB database nyt
+  getSavedArticles: function() {
+    return axios.get('/api/articles')
+  },
+  // Saves the selected article to MongoDB database nyt 
+  saveArticle: function(articleData) {
+  	return axios.post('/api/articles', articleData)
+  },
+  deleteArticle: function(id) {
+    return axios.delete('/api/articles/' + id)
+  },
 }
