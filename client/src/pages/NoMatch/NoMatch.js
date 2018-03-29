0babf43496a4171f892066b5202236b53a4c1ec4
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-28 21:43:21 -0700

    Added pages folder

--- client/src/pages/NoMatch/NoMatch.js
+++ client/src/pages/NoMatch/NoMatch.js
@@ -1,22 +1,17 @@
-import React from "react";
-import { Col, Row, Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
+import React from 'react'
+import { Col, Container, Row } from '../../components/Grid'
+import Subtitle from '../../components/Subtitle'
 
 const NoMatch = () => (
   <Container fluid>
     <Row>
-      <Col size="md-12">
-        <Jumbotron>
+      <Col size='md-12'>
+        <Subtitle>
           <h1>404 Page Not Found</h1>
-          <h1>
-            <span role="img" aria-label="Face With Rolling Eyes Emoji">
-              🙄
-            </span>
-          </h1>
-        </Jumbotron>
+        </Subtitle>
       </Col>
     </Row>
   </Container>
-);
+)
 
-export default NoMatch;
+export default NoMatch
