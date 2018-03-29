0aef0312f60015c6ccdcf9294510ced7ebcc0b4b
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-29 15:27:53 -0700

    added remove function

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -16,4 +16,11 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
+  remove: function(req, res) {
+  	db.Article
+  		.findById({ _id: req.params.id })
+  		.then(dbModel => dbModel.remove())
+  		.then(dbModel => res.json(dbModel))
+  		.catch(err => res.status(422).json(err))
+  },
 }
