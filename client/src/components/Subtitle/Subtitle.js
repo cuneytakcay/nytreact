c99476374e42c55a1cb8d206d713ea168946a6f9
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-28 21:15:03 -0700

    modified Nav and Subtitle components

--- client/src/components/Subtitle/Subtitle.js
+++ client/src/components/Subtitle/Subtitle.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
-class Subtitle extends React.Component {
-  render() {
-  	return (
-  		<div style={{ height: 100, clear: 'both' }}>
-    		{children}
-  		</div>
-  	)
-	}
-}
+const Subtitle = props => (
+	<div style={{ height: 100, clear: 'both' }}>
+		{children}
+	</div>
+)
 
 export default Subtitle 
