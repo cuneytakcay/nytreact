13025e97e800e0cded75e65e9a1a0789171cac6d
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-03-31 21:43:17 -0700

    Added date functionality to the API.js for search query

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-const url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
-const apiKey = '&api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931'
+const url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?'
+const apiKey = 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931'
 
 export default {
 	// Gets last 10 matching articles from NYT API
-  getArticles: function(query) {
-    return axios.get(url + query + apiKey)
+  getArticles: function(query, beginDate, endDate) {
+    return axios.get(`${url}q=${query}&begin_date=${beginDate}0101&end_date=${endDate}0101&api-key=${apiKey}`)
   }, 
   // Gets all saved articles from MongoDB database nyt
   getSavedArticles: function() {
