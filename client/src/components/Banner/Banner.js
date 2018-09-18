9c478ebd1e584510886a05cc2cb7a0dcb1d3a7b3
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-09-17 22:56:44 -0700

    removed banner items

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -20,23 +20,10 @@ class Banner extends React.Component {
   render() {
     return (
       <Container fluid>
-      <Navbar className='banner' color='dark' dark expand='md'>
-        <NavbarBrand href='/' className='mr-auto ml-5 pl-4'><h1>NYT Article Search</h1></NavbarBrand>
-        <NavbarToggler onClick={this.toggleNavbar} className='toggler ml-3 mr-5' />
-        <Collapse isOpen={!this.state.collapsed} navbar>
-          <Nav className='ml-auto mr-5 pr-4' navbar>
-            <NavItem>
-              <NavLink href='/'>Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/login/'>Log in</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/signup/'>Sign up</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar></Container>
+        <Navbar className='banner' color='dark' dark expand='md'>
+          <NavbarBrand href='/' className='mr-auto ml-5 pl-4'><h1>NYT Article Search</h1></NavbarBrand>
+        </Navbar>
+      </Container>
     )
   }
 }
