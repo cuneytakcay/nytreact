0e5af7d95271b4dba0b90c7937f5127ab9023ac7
Cuneyt Akcay <cuneytakcay@gmail.com>
2018-10-03 21:41:03 -0700

    Fixed indentations.

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -3,12 +3,12 @@ const db = require('../models')
 
 // Define methods
 module.exports = {
-	findAll: function(req, res) {
-		db.Article
-			.find()
-			.sort({ date: -1 })
-			.then(dbModel => res.json(dbModel))
-			.catch(err => res.status(422).json(err))
+  findAll: function(req, res) {
+    db.Article
+      .find()
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
 	},
   create: function(req, res) {
     db.Article
@@ -17,10 +17,10 @@ module.exports = {
       .catch(err => res.status(422).json(err))
   },
   remove: function(req, res) {
-  	db.Article
-  		.findById({ _id: req.params.id })
-  		.then(dbModel => dbModel.remove())
-  		.then(dbModel => res.json(dbModel))
-  		.catch(err => res.status(422).json(err))
+    db.Article
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   },
 }
